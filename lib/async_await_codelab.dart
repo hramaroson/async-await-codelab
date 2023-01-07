91ab4c54550a434e4ffa2d8921de8b0ac7ec12cb
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-07 22:16:18 +0300

    Improve delay  management

--- lib/async_await_codelab.dart
+++ lib/async_await_codelab.dart
@@ -1,19 +1,25 @@
 import 'package:chalkdart/chalk.dart'; 
 
-Future<void> printOrderMessage() async{
+Future<void> printOrderMessage({int delay=2}) async{
   print(chalk.green("Fetching order ..."));
-  var order = await fetchUserOrder();
+  var order = await fetchUserOrder(delay: delay);
   print(chalk.magenta("Your order is $order"));
 }
 
-Future<String> fetchUserOrder() {
-  return Future.delayed(const Duration(seconds: 2), 
+Future<String> fetchUserOrder({int delay=2}) {
+  return Future.delayed(Duration(seconds: delay), 
     () => "Large Latte");
 }
 
 Future<void> fetchUserOrderWithError(){
   return Future.delayed(const Duration(seconds: 2), 
     () => throw Exception("Logout failed: user ID is invalid"));
+}
+
+void countSeconds (int secs){
+  for(var i = 1; i<=secs; i++){
+    Future.delayed(Duration(seconds: i) , () => print("$i"));
+  }
 }
 
 Future<int> execute() async {
@@ -28,6 +34,10 @@ Future<int> execute() async {
  } catch(e) {
   print(chalk.brightRed("$e"));
  }
+
+ print(chalk.blue("printOrderMessage() and counting delay (4 secs)"));
+ countSeconds(4);
+ await printOrderMessage(delay: 4);
  
 
  return 0;
